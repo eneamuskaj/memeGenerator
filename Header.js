5b903aaeea8841f5410ad0071b6166a4f7d766a4
eneamuskaj <eneamuskaj@gmail.com>
2021-06-16 21:43:19 -0500

    commit

---
+++ Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Header extends React.Component {
+    render () {
+        return (
+            <header>
+            <img src="https://ih1.redbubble.net/image.493812748.1150/poster,840x830,f8f8f8-pad,1000x1000,f8f8f8.u3.jpg" alt="memeImg" />
+            <p>Meme Generator </p>
+            </header>
+        )
+    }
+}
+
+export default Header
